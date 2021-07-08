36d6c4f08af50d2a564ee58339403b54e9ac6fec
Bram Vandewalle <bram.vandewalle@into.care>
2021-07-08 11:11:40 +0200

    Determine KATZ score an sich & with bedtypes and diagnoses

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const getCategory = (score, dementia) => {
+const getBasicCategory = score => {
 	if (score.length === 6) {
 		score += '11';
 	}
@@ -19,30 +19,22 @@ const getCategory = (score, dementia) => {
 
 	if (
 		washingAndClothing &&
-		(timeAndSpace || dementia) &&
+		timeAndSpace &&
 		continence >= 3 &&
 		(transfers >= 3 || toilet >= 3 || meal >= 3)
 	) {
-		return 'CD';
+		return 'Cd';
 	}
 
 	if (washingAndClothing && transfers >= 3 && toilet >= 3 && (continence >= 3 || meal >= 3)) {
-		if (dementia) {
-			return 'CD';
-		}
-
 		return 'C';
 	}
 
 	if (washingAndClothing && (transfers >= 3 || toilet >= 3 || timeAndSpace)) {
-		if (dementia) {
-			return 'D';
-		}
-
 		return 'B';
 	}
 
-	if (washingOrClothing && !dementia) {
+	if (washingOrClothing) {
 		if (timeAndSpace) {
 			return 'B';
 		}
@@ -50,15 +42,145 @@ const getCategory = (score, dementia) => {
 		return 'A';
 	}
 
-	if (timeAndSpace && !dementia) {
+	if (timeAndSpace) {
 		return 'A';
 	}
 
-	if (dementia) {
-		return 'D';
+	return 'O';
+};
+
+const isIncontinent = score => {
+	const toilet = score.charAt(3);
+
+	return toilet >= 3;
+};
+
+const isIncontinentAndNeedHelpsWithTransferFoodOrToilet = score => {
+	// eslint-disable-next-line no-unused-vars
+	const [washing, clothing, transfers, toilet, continence, meal, time, space] = score;
+
+	return (continence >= 3 && (transfers >= 3 || meal >= 3 || toilet >= 3));
+};
+
+const getCategoryForOBed = (bedType, dementia, palliative) => {
+	if (bedType === 'RVT') {
+		throw new Error('O score cannot be placed in RVT bed');
 	}
 
-	return 'O';
+	if (bedType === 'DVC' && !dementia) {
+		throw new Error('O score cannot be placed in DVC bed if no dementia diagnosis exists');
+	}
+
+	if (bedType === 'DVC' && dementia) {
+		return palliative ? 'Fp' : 'D';
+	}
+
+	// ROB+CVK checks
+	return dementia ? 'D' : 'O';
+};
+
+const getCategoryForABed = (score, bedType, dementia, palliative) => {
+	if (bedType === 'DVC' && !dementia) {
+		throw new Error('A score cannot be placed in DVC bed if no dementia diagnosis exists');
+	}
+
+	if (bedType === 'DVC' && dementia) {
+		return palliative ? 'Fp' : 'D';
+	}
+
+	const isIncontinentAndNeedsExtraHelp = isIncontinentAndNeedHelpsWithTransferFoodOrToilet(score);
+	if (bedType === 'RVT') {
+		if (!dementia || !isIncontinentAndNeedsExtraHelp) {
+			throw new Error('A score cannot be placed in RVT bed if no dementia diagnosis exists or incontinence and additional help');
+		}
+
+		return 'Cd';
+	}
+
+	// ROB+CVK checks
+	if (!dementia) {
+		return 'A';
+	}
+
+	return isIncontinentAndNeedsExtraHelp ? 'Cd' : 'D';
+};
+
+const getCategoryForBBed = (score, bedType, dementia, palliative) => {
+	if (bedType === 'DVC') {
+		if (palliative) {
+			return 'Fp';
+		}
+
+		return dementia ? 'D' : 'F';
+	}
+
+	const isIncontinentAndNeedsExtraHelp = isIncontinentAndNeedHelpsWithTransferFoodOrToilet(score);
+	if (bedType === 'RVT') {
+		if (!dementia) {
+			return 'B';
+		}
+
+		return isIncontinentAndNeedsExtraHelp ? 'Cd' : 'B';
+	}
+
+	// ROB+CVK checks
+	if (!dementia) {
+		return 'B';
+	}
+
+	return isIncontinentAndNeedsExtraHelp ? 'Cd' : 'D';
+};
+
+const getCategoryForCBed = (score, bedType, dementia, palliative) => {
+	if (bedType === 'DVC') {
+		if (palliative) {
+			return 'Fp';
+		}
+
+		return dementia ? 'D' : 'F';
+	}
+
+	// ROB+CVK+RVT checks
+	if (!dementia) {
+		return 'C';
+	}
+
+	return isIncontinent(score) ? 'Cd' : 'D';
+};
+
+const getCategoryForCdBed = (bedType, dementia, palliative) => {
+	if (bedType === 'DVC') {
+		if (palliative) {
+			return 'Fp';
+		}
+
+		return dementia ? 'D' : 'F';
+	}
+
+	return 'Cd';
+};
+
+const getCategory = (score, bedType, dementia = false, palliative = false) => {
+	const basicCategory = getBasicCategory(score);
+
+	if (bedType && bedType.toUpperCase() === 'NO') {
+		return basicCategory;
+	}
+
+	switch (basicCategory) {
+		case 'A':
+			return getCategoryForABed(score, bedType, dementia, palliative);
+		case 'B':
+			return getCategoryForBBed(score, bedType, dementia, palliative);
+		case 'C':
+			return getCategoryForCBed(score, bedType, dementia, palliative);
+		case 'Cd':
+			return getCategoryForCdBed(bedType, dementia, palliative);
+		case 'O':
+			return getCategoryForOBed(bedType, dementia, palliative);
+		default:
+			return basicCategory;
+	}
 };
 
 module.exports = getCategory;
